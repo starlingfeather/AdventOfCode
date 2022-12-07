6220a2b43960bd59e52e1ac99d93591f99b22a2c
Ambar Diaz <ambard@amazon.com>
2022-12-06 18:20:08 -0800

    add: day 4 test

--- 2022/4/test.py
+++ 2022/4/test.py
@@ -6,10 +6,14 @@ from code import *
 
 
 class TestAOC(unittest.TestCase):
-
     def test(self):
-        example = None
-        answer = None
+        example = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+        answer = 2
         self.assertEqual(answer, solve_part_one(example))
 
 
